c748ea17e347daa7dc10e257fd80df2f1bd9a049
Leonardodarienzo <darienzo.leonardo@iisgalvanimi.edu.it>
2024-12-11 12:41:18 +0000

    errori immagini

--- app.py
+++ app.py
@@ -2,42 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Rotte principali
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/servizi.html")
-def servizi():
-    return render_template("servizi.html")
+@app.route('/storia')
+def history():
+    return render_template('storia.html')
 
-@app.route("/documenti.html")
-def documenti():
-    return render_template("documenti.html")
+@app.route('/attrazioni')
+def attractions():
+    return render_template('attrazioni.html')
 
-@app.route("/tributi.html")
-def tributi():
-    return render_template("tributi.html")
+@app.route('/eventi')
+def events():
+    return render_template('turismo.html')
 
-@app.route("/anagrafe.html")
-def anagrafe():
-    return render_template("anagrafe.html")
-
-@app.route("/turismo.html")
-def turismo():
-    return render_template("turismo.html")
-
-@app.route("/notizie.html")
-def notizie():
-    return render_template("notizie.html")
-
-@app.route("/attrazioni.html")
-def attrazioni():
-    return render_template("attrazioni.html")
-
-@app.route("/storia.html")
-def storia():
-    return render_template("storia.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
